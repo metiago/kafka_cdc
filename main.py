231c0f65a72661a83767ddb17ae10536b49e3041
tiago <tiagotg.ribeiro@gmail.com>
2024-08-04 16:15:46 -0300

    breaking down in functions

--- main.py
+++ main.py
@@ -8,36 +8,41 @@ conf = {
 }
 
 consumer = Consumer(conf)
-topic = 'mysql-.inventory.customers'
+topic = 'mysql-.inventory.langs'
 consumer.subscribe([topic])
 
-try:
-    while True:
-        # Timeout of 1 second
-        msg = consumer.poll(1.0)
+def main():
+    try:
+        while True:
+            # Timeout of 1 second
+            msg = consumer.poll(1.0)
 
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                print('End of partition event')
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('End of partition event')
+                else:
+                    print(f'Error: {msg.error()}')
             else:
-                print(f'Error: {msg.error()}')
-        else:
-            message = json.loads(msg.value().decode("utf-8"))
-            payload = message.get("payload")
-            _id = payload.get("after").get("id")
-            name = payload.get("after").get("first_name")
-            print(f"Language id: {_id} and name: {name}")
+                message = json.loads(msg.value().decode("utf-8"))
+                payload = message.get("payload")
+                _id = payload.get("after").get("id")
+                name = payload.get("after").get("first_name")
+                print_languages(message)
 
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close()
 
 
 def print_languages(message):
     payload = message.get("payload")
     _id = payload.get("after").get("id")
     name = payload.get("after").get("name")
-    print(f"Language id: {_id} and name: {name}")
+    print(f"Language id: {_id}, name: {name}")
+
+
+if __name__ == "__main__":
+    main()
